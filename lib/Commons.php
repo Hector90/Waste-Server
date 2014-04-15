050445447d20ca3f6978e10ff6db4aa5cf00a8a2
hsopanen <hsopanen@gmail.com>
2014-04-15 11:27:16 +0300

    Fixes for Commons

--- lib/Commons.php
+++ lib/Commons.php
@@ -10,13 +10,13 @@ class Commons {
             405 => 'Method Not Allowed',
             500 => 'Internal Server Error',
         ); 
-        return ($status[$state])?$status[$state]:$status[500];	
+        return (array_key_exists($state, $status)) ? $status[$state] : $status[500];	
 	}
 	
 	public static function getParam($name, $api, $pathIndex) { 
 		$val = ($_SERVER['REQUEST_METHOD'] == 'POST' && array_key_exists($name, $_POST) ? $_POST[$name] : ($_SERVER['REQUEST_METHOD'] != 'POST' && array_key_exists($name, $_GET) ? $_GET[$name] : NULL));
 		if ($val == NULL && $api != NULL && $pathIndex != NULL && count($api->path_parts) >= $pathIndex) {
-			$val = $api->path_parts[$pathIndex];
+			$val = isset($api->path_parts[$pathIndex]) ? $api->path_parts[$pathIndex] : NULL;
 		}
 		return $val;
 	}
